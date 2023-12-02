bbe15ba9df807561b170b55500cfa03e971a411f
ajeetkumar2468 <152690858+ajeetkumar2468@users.noreply.github.com>
2023-12-02 15:28:30 +0530

    Add files via upload
    First Commit

---
+++ Straight_triangle.java
@@ -0,0 +1,18 @@
+package Paterns_Star;
+//p60
+public class Straight_triangle {
+	public static void main(String[] args) {
+		
+	
+	for(int x = 1;x<=5;x++)
+	{
+		for(int y = 1;y<=x;y++)
+		{
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+	
+	}
+
+}
